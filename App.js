deadb43daca0960cfd74ca7bfa8d97bc87bd5b80
Tom Gallagher <tom.gallagher@theappengineers.com>
2022-01-04 21:05:08 +0000

    basic listview

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, SafeAreaView, useColorScheme } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import ListView from './src/ListView'
 
 export default function App() {
+  const colorScheme = useColorScheme()
+  const themeContainerStyle = colorScheme === 'light' ? styles.lightContainer : styles.darkContainer
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={themeContainerStyle}>
       <StatusBar style="auto" />
+      <SafeAreaView edges={['top']} style={styles.safeHeader} />
+      <ListView />
+      <SafeAreaView edges={['bottom']} style={styles.safeFooter} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  darkContainer: {
+    backgroundColor: '#b80000',
+    flex: 1
   },
+  lightContainer: {
+    backgroundColor: '#db3e00',
+    flex: 1
+  },
+  safeHeader: {
+    backgroundColor: '#008b02'
+  },
+  safeFooter: {
+    backgroundColor: '#006b76',
+    marginTop: -1
+  }
 });
